27110ef9f7edcff9b31383ee93de2b4e68bce9c0
Simon Harms <simon@simonharms.com>
2026-06-24 18:14:25 +0000

    chore(release): v0.12.0

--- packages/cli/src/stubs-project.ts
+++ packages/cli/src/stubs-project.ts
@@ -1,6 +1,6 @@
 import type { NewProjectOptions } from './new-project-options.js';
 
-const CORE_VERSION = '^0.11.0';
+const CORE_VERSION = '^0.12.0';
 
 export function projectPackageJson(name: string, options: NewProjectOptions): string {
   const dependencies: Record<string, string> = {
